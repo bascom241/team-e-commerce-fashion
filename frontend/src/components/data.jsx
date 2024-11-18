6162e4f981ca576448bca96df4b5729d564eb89f
bascom241 <abdulbasitabdulwahab21@gmail.com>
2024-11-18 17:36:35 +0100

    commiting Some Changes

---
+++ frontend/src/components/data.jsx
@@ -0,0 +1,70 @@
+// Fashion Products Data
+const products = [
+    {
+      id: 1,
+      name: "Red Dress",
+      price: 100,
+      category: "Apparel",   // Type of product
+      quantity: 20,
+      type: "Dress",         // Specific product type
+    },
+    {
+      id: 2,
+      name: "Blue Jeans",
+      price: 40,
+      category: "Apparel",
+      quantity: 50,
+      type: "Pants",
+    },
+    {
+      id: 3,
+      name: "Black Boots",
+      price: 80,
+      category: "Footwear",
+      quantity: 15,
+      type: "Boots",
+    },
+    {
+      id: 4,
+      name: "White Sneakers",
+      price: 60,
+      category: "Footwear",
+      quantity: 25,
+      type: "Sneakers",
+    },
+    {
+      id: 5,
+      name: "Leather Jacket",
+      price: 120,
+      category: "Apparel",
+      quantity: 30,
+      type: "Jacket",
+    },
+    {
+      id: 6,
+      name: "Sunglasses",
+      price: 40,
+      category: "Accessories",
+      quantity: 100,
+      type: "Sunglasses",
+    },
+    {
+      id: 7,
+      name: "Hat",
+      price: 25,
+      category: "Accessories",
+      quantity: 60,
+      type: "Hat",
+    },
+    {
+      id: 8,
+      name: "Wristwatch",
+      price: 150,
+      category: "Accessories",
+      quantity: 40,
+      type: "Watch",
+    },
+  ];
+  
+  export default products;
+  
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Data from '../../components/data'
+import { useState } from 'react'
 const Home = () => {
+
+  const [products, setProducts] = useState(Data);
   return (
     <div>
-      
+      <div>
+
+      </div>
+
+      <div>
+        <div className='text-center'>
+          <h1 className='text-5xl font-bold text-gray-600 mb-3'>Classifications</h1>
+          <p className='text-gray-600'>Finding Best Products now in your finger tips</p>
+        </div>
+        {products.map(p => (
+          <div>
+            <img src={p.img}/>
+        </div>
+        ))}
+      </div>
     </div>
   )
 }
