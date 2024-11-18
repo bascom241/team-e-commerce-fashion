a37776f3f73c4942bf23afef56078bb81fef9830
zainah7 <zainaabd7@gmail.com>
2024-11-18 17:34:17 +0100

    added new fonts

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-
 import React from "react";
 import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import { BsPersonFillCheck } from "react-icons/bs";
 
-
 const Navbar = () => {
   return (
-    <div className="p-10">
+    <div className="p-10 px-24">
       <div className="flex justify-between">
         <p className="text-4xl font-semibold">Fashionista</p>
         <div className="flex">
-          <input type="text" placeholder="Search" />
+          <div className="">
+            <input type="text" placeholder="Search" className="" />
+          </div>
           <div className="flex gap-3">
             <FaShoppingCart size={30} />
             <FaHeart size={30} />
@@ -19,8 +19,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
-
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <div>
-
-      </div>
+      <div></div>
 
       <div>
-      <div>
-      <h1>Classifications</h1>
-      <p>Finding best Products now in your finger tips </p>
-      </div>
-   
-        
+        <div></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
