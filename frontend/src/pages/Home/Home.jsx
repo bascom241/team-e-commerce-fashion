9794ad79669577a4e9a50770da457cbf950cec8d
bascom241 <abdulbasitabdulwahab21@gmail.com>
2024-11-18 16:56:09 +0100

    NavBar Changes
    Commiting navbar Changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <div>
+
+      </div>
+
+      <div>
+      <div>
+      <h1>Classifications</h1>
+      <p>Finding best Products now in your finger tips </p>
+      </div>
+   
+        
+      </div>
     </div>
   )
 }
