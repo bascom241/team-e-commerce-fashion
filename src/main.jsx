08acce6ae53b96f0387d893acb63085261d37141
yahayagodiwa <yahayagodiwa8@gmail.com>
2024-11-20 16:17:31 +0100

    Adding Classfication component

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LayOut from './components/LayOut.jsx';
+import Home from './pages/Home/Home.jsx';
+
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <LayOut />,
+  children: [
+    { index: true, element: <Home /> }
+  ]
+}])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
