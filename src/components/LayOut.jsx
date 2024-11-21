8f1b2ffb036ecbe6d25bba21337109357c11166d
bascom241 <abdulbasitabdulwahab21@gmail.com>
2024-11-21 15:30:16 +0100

    Update LayOut.jsx

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
-import Footer from "./Footer/Footer";
+import Footer from './Footer/Footer'
 const LayOut = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 };
